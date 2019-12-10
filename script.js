6356d239211a81701e0f916c4f237c10be79976e
nasjones <nasjonez1@gmail.com>
2019-12-10 10:12:00 -0500

    location update and mobile style changes

--- script.js
+++ script.js
@@ -9,16 +9,15 @@ function initialize() {
 }
 
 function placesLoad(search) {
-    console.log("initializing");
-    var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
+    var location = new google.maps.LatLng($.get('https://ipapi.co/latitude/'), $.get('https://ipapi.co/longitude/'));
 
     map = new google.maps.Map(document.getElementById('map'), {
-        center: pyrmont,
+        center: location,
         zoom: 15
     });
 
     var request = {
-        location: pyrmont,
+        location: location,
         radius: '500',
         query: search
     };
@@ -32,10 +31,8 @@ function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < results.length, i < 10; i++) {
             var place = results[i];
-            $('#restaurants').append(`<li>${place.name}<br>${place.formatted_address}<br></li>`)
-            console.log(place);
+            $('#restaurants').append(`<li>${place.name}<br>${place.formatted_address}<br></li><hr>`)
         }
-        console.log("done");
     }
 }
 
@@ -43,28 +40,26 @@ function recipesLoad(search) {
     $.ajax({
         "async": true,
         "crossDomain": true,
-        "url": "https://api.edamam.com/search?q=" + search + "&app_id=24c87adf&app_key=69ca0b9b101ea652dd98fdcc6f0c3205&callback=?&from=0&to=10",
+        "url": "https://api.edamam.com/search?q=" + search + "&app_id=24c87adf&app_key=69ca0b9b101ea652dd98fdcc6f0c3205&callback=?",
         "method": "GET",
         "SameSite": "none",
         jsonpCallback: 'jsonCallback',
         contentType: "application/json",
         dataType: 'jsonp',
         success: function (results) {
-            console.log(results.hits);
             recipePrinter(results);
         },
         error: function (e) {
             console.log(e.message);
-
         }
     });
 }
 
 function recipePrinter(results) {
+    $('#recipes').empty();
     for (var i = 0; i < results.hits.length, i < 10; i++) {
         var recipe = results.hits[i].recipe;
-        console.log(results.hits[i].recipe.label);
-        $('#recipes').append(`<li>${recipe.label}<br><a href="${recipe.url}" target="_blank">Recipe Link!<a/></li>`)
+        $('#recipes').append(`<li>${recipe.label}<br><a href="${recipe.url}" target="_blank">Recipe Link!<a/></li><hr>`)
     }
 }
 
@@ -72,6 +67,7 @@ function submit() {
     $('form').submit(e => {
         e.preventDefault();
         initialize();
+        $('.output-box').css('display', 'inherit')
     });
 }
 
